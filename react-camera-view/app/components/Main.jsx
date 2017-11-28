de0b077575c6dd4a0bd789031f4373fa2ea1394f
dtthor <d.t.thorwaldson@gmail.com>
2017-11-28 13:17:01 -0600

    bring in axios and write api call

--- react-camera-view/app/components/Main.jsx
+++ react-camera-view/app/components/Main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const Main = React.createClass({
   getInitialState() {
     return {cart: [], cameras: []}
   },
 
-  
+  componentDidMount() {
+    axios.get('https://guarded-cliffs-16411.herokuapp.com/cameras')
+      .then(res => {
+        this.setState({cameras: res.data});
+      });
+  },
+
   render() {
     return (
       <main>
